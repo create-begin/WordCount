bd0773357e9b75a736e27f3681270d6bb9f87394
create-begin <3496280978@qq.com>
2023-05-06 20:18:46 +0800

    编码调试

--- WordCount/WordCount/WordCount.cpp
+++ WordCount/WordCount/WordCount.cpp
@@ -1,20 +1,23 @@
-﻿// WordCount.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <bits/stdc++.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char a[100000];//用于读取文本的内容
+	int cnt_1 = 0;//单词数
+	int cnt_2 = 0;//字符数
+	ifstream input;
+	input.open("test.txt");//打开文件	
+	input.getline(a, 100);
+	int l = strlen(a);
+	for (int i = 0; i < l; i++) {
+		if (a[i] == ' '||a[i]==',') cnt_1++;//记录单词数
+		else cnt_2++;//记录字符数
+	}
+	cout << "单词数为：" << cnt_1 << endl;
+	cout << "字符数为：" << cnt_2 << endl;
+	input.close();//关闭文件
+    return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
